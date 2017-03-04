55022244d7c8454383565752133c440201e4d8ad
David Germain <davidispizza@gmail.com>
2017-03-04 01:14:41 -0500

    Change bools to chars

--- java/MagicSquare/src/Main.java
+++ java/MagicSquare/src/Main.java
@@ -1,26 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
   
-  
   public static void main(String args[]) throws FileNotFoundException {
     Scanner in = new Scanner(new File("prob1.in"));
     
-    boolean[] goal = {
-        true, true, true,
-        true, false, true,
-        true, true, true
-      };
+    char[] GOAL = {
+      '1', '1', '1',
+      '1', '0', '1',
+      '1', '1', '1'
+    };
+    
+    Set<Integer> pool = new HashSet<>();
+    for (int i = 1; i < 10; i++) {
+      pool.add(i);
+    }
+    Set<Set<Integer>> COMBOS = powerSet(pool);
+
     
     while (in.hasNextLine()) {
-      boolean[] grid;
+      char[] grid;
       try {
         String line1 = in.nextLine();
         String line2 = in.nextLine();
@@ -31,44 +38,33 @@ public class Main {
         break;
       }
       
-      Set<Integer> pool = new HashSet<>();
-      for (int i = 1; i < 10; i++) {
-        pool.add(i);
+      for (Set<Integer> combo : COMBOS) {
+        char[] table = grid.clone();
+        for (int value : combo) {
+          translate(table, value);
+        }
+        if (isSame(table, GOAL)) {
+          System.out.println(combo);
+        }
       }
-      Set<Set<Integer>> combos = powerSet(pool);
-      
-      int lengthOfCurrentSolution = 1000000;
-      Set<Integer> solution = new HashSet<>();
-      for (Set<Integer> combo : combos) {
-        if (combo.size() < lengthOfCurrentSolution) {
-          boolean[] table = grid.clone();
-          for (int value : combo) {
-            translate(table, value);
-          }
-          if (isSame(table, goal)) {
-            lengthOfCurrentSolution = combo.size();
-            solution = combo;
-          }
-        }      
-      }
-      System.out.println(solution);
     }
+    in.close();
   }
   
-  private static boolean[] setup(String line1, String line2, String line3) {
-    boolean[] grid = new boolean[9];
+  private static char[] setup(String line1, String line2, String line3) {
+    char[] grid = new char[9];
     
-    grid[0] = line1.charAt(0) == '1';
-    grid[1] = line1.charAt(1) == '1';
-    grid[2] = line1.charAt(2) == '1';
+    grid[0] = line1.charAt(0);
+    grid[1] = line1.charAt(1);
+    grid[2] = line1.charAt(2);
     
-    grid[3] = line2.charAt(0) == '1';
-    grid[4] = line2.charAt(1) == '1';
-    grid[5] = line2.charAt(2) == '1';
+    grid[3] = line2.charAt(0);
+    grid[4] = line2.charAt(1);
+    grid[5] = line2.charAt(2);
     
-    grid[6] = line3.charAt(0) == '1';
-    grid[7] = line3.charAt(1) == '1';
-    grid[8] = line3.charAt(2) == '1';
+    grid[6] = line3.charAt(0);
+    grid[7] = line3.charAt(1);
+    grid[8] = line3.charAt(2);
     
     return grid;
   }
@@ -92,7 +88,7 @@ public class Main {
     return sets;
   }
 
-  private static boolean isSame(boolean[] grid, boolean[] s) {
+  private static boolean isSame(char[] grid, char[] s) {
     if (grid.length != s.length) {
       return false;
     }
@@ -104,45 +100,39 @@ public class Main {
     return true;
   }
 
-  private static void translate(boolean[] grid, int i) {
-    switch (i) {
-    case 1:
-      flip(grid, new int[]{1, 2, 4, 5});
-      break;
-    case 2:
-      flip(grid, new int[]{1, 2, 3});
-      break;
-    case 3:
-      flip(grid, new int[]{2, 3, 5, 6});
-      break;
-    case 4:
-      flip(grid, new int[]{1, 4, 7});
-      break;
-    case 5:
-      flip(grid, new int[]{2, 4, 5, 6, 8});
-      break;
-    case 6:
-      flip(grid, new int[]{3, 6, 9});
-      break;
-    case 7:
-      flip(grid, new int[]{4, 5, 7, 8});
-      break;
-    case 8:
-      flip(grid, new int[]{7, 8, 9});
-      break;
-    case 9:
-      flip(grid, new int[]{5, 6, 8, 9});
-      break;
-    default:
-        break;
-    }
+  private static void translate(char[] grid, int i) {
+    Map<Integer, int[]> rules = new HashMap<Integer, int[]>();
+    
+    // corners
+    rules.put(1, new int[]{1, 2, 4, 5});
+    rules.put(3, new int[]{2, 3, 5, 6});
+    rules.put(7, new int[]{4, 5, 7, 8});
+    rules.put(9, new int[]{5, 6, 8, 9});
+    
+    // edges
+    rules.put(2, new int[]{1, 2, 3});
+    rules.put(4, new int[]{1, 4, 7});
+    rules.put(6, new int[]{3, 6, 9});
+    rules.put(8, new int[]{7, 8, 9});
+    
+    // middle
+    rules.put(5,  new int[]{2, 4, 5, 6, 8});
+
+    flip(grid, rules.get(i));
   }
 
-  private static void flip(boolean[] grid, int[] flips) {
+  private static void flip(char[] grid, int[] flips) {
     for (int i = 0; i < flips.length; i++) {
       int flip = flips[i];
       int index = flip - 1;
-      grid[index] = !grid[index];
+      grid[index] = flip(grid[index]);
     }
+  }
+
+  private static char flip(char c) {
+    if (c == '0') {
+      return '1';
+    }
+    return '0';
   }  
 }
