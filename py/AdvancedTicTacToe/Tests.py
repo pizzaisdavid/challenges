d15b53f6d8676b7850a9623fd4ec5d5ec77fe7bf
David Germain <davidispizza@gmail.com>
2017-03-09 17:53:23 -0500

    Solve adv tic tac toe

--- py/AdvancedTicTacToe/Tests.py
+++ py/AdvancedTicTacToe/Tests.py
@@ -1,4 +1,142 @@
-
-import unittest
 import main
+import unittest
 
+class TestSumEqualsProduct(unittest.TestCase):
+
+    def test_fits_segmentIsOne(self):
+        width = 5
+        segment = 1
+        expected = 5
+        answer = main.fits(width, segment)
+        self.assertEqual(answer, expected)
+
+    def test_fits_segmentIsLonger(self):
+        width = 3
+        segment = 4
+        expected = 0
+        answer = main.fits(width, segment)
+        self.assertEqual(answer, expected)
+
+    def test_fits_sameLength(self):
+        width = 3
+        segment = 3
+        expected = 1
+        answer = main.fits(width, segment)
+        self.assertEqual(answer, expected)
+
+    def test_fits_segmentIsHalfLength(self):
+        width = 6
+        segment = 3
+        expected = 4
+        answer = main.fits(width, segment)
+        self.assertEqual(answer, expected)
+
+    def test_fits_lessThenHalf(self):
+        width = 5
+        segment = 2
+        expected = 4
+        answer = main.fits(width, segment)
+        self.assertEqual(answer, expected)
+
+
+    def test_sqaureFits_SquareIsOne(self):
+        totalHeight = 4
+        totalWidth = 4
+        segmentHeight = 2
+        segmentWidth = 2
+        expected = 9
+        answer = main.squareFits(totalHeight, totalWidth, segmentHeight, segmentWidth)
+        self.assertEqual(answer, expected)
+
+    def test_sqaureFits_halfLength(self):
+        totalHeight = 4
+        totalWidth = 4
+        segmentHeight = 2
+        segmentWidth = 2
+        expected = 9
+        answer = main.squareFits(totalHeight, totalWidth, segmentHeight, segmentWidth)
+        self.assertEqual(answer, expected)
+
+    def test_1(self):
+        r = 3
+        c = 3
+        k = 3
+        expected = 8
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+
+    def test_2(self):
+        r = 3
+        c = 3
+        k = 2
+        expected = 20
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_3(self):
+        r = 3
+        c = 4
+        k = 1
+        expected = 12
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_4(self):
+        r = 1
+        c = 5
+        k = 5
+        expected = 1
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_5(self):
+        r = 1
+        c = 5
+        k = 3
+        expected = 3
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_6(self):
+        r = 5
+        c = 1
+        k = 5
+        expected = 1
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_7(self):
+        r = 5
+        c = 1
+        k = 3
+        expected = 3
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_8(self):
+        r = 3
+        c = 4
+        k = 5
+        expected = 0
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_9(self):
+        r = 12
+        c = 10
+        k = 8
+        expected = 116
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+    def test_10(self):
+        r = 97
+        c = 83
+        k = 5
+        expected = 30076
+        answer = main.count(r, c, k)
+        self.assertEqual(answer, expected)
+
+if __name__ == '__main__':
+    unittest.main()
--- py/AdvancedTicTacToe/main.py
+++ py/AdvancedTicTacToe/main.py
@@ -1,24 +1,53 @@
 
 def main():
-    r = 3
-    c = 3
-    k = 2
 
-    answer = count(r, c, k)
-    print(answer)
+    with open("prob5.in", "r") as inFile, open("prob5.out", "w+") as outFile:
+        while True:
+            try:
+                line = inFile.readline()
+                while line == "\n":
+                    line = inFile.readline()
+                    
+                tokens = line.split()
+                print(tokens)
+                if len(tokens) == 1 and tokens[0] == "0":
+                    print("DONE")
+                    break
+                r, c, k = [int(x) for x in tokens]
+                answer = count(r, c, k)
+                outFile.write(str(answer) + "\n")
+                print(answer)
+            except EOFError:
+                print("Done!!")
 
 def count(rows, columns, length):
-    total = 0
-    total += fits(rows, length) * columns
-    total += fits(columns, length) * rows
-    total += squareFits(rows, columns, length, length) * 2
-    return total
+    if length == 1:
+        return rows * columns
+    else:
+        total = 0
+        total += fits(rows, length) * columns
+        total += fits(columns, length) * rows
+        total += squareFits(rows, columns, length, length) * 2
+        return total
 
 def fits(totalLength, segmentLength):
-    return 0
+    starts = []
+    for start in range(0, totalLength):
+        if start + segmentLength <= totalLength and start not in starts:
+            starts.append(start)
+    return len(starts)
+        
 
 def squareFits(heightTotal, widthTotal, smallHeight, smallWidth):
-    return 0
+    starts = []
+    for start_height in range(0, heightTotal):
+        for start_width in range(0, widthTotal):
+            end_height = start_height + smallHeight
+            end_width = start_width + smallWidth
+            pair = (start_height, start_width)
+            if end_height <= heightTotal and end_width <= widthTotal and pair not in starts:
+                starts.append(pair)
+    return len(starts)
 
 if __name__ == "__main__":
     main()
